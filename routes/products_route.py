d40fca94f62e777b03b2b067b154f7105e3cb437
Adegoju5 <115003200+Adegoju5@users.noreply.github.com>
2024-06-23 01:02:04 +0200

    general changes

--- routes/products_route.py
+++ routes/products_route.py
@@ -3,13 +3,11 @@ from models.products import Product
 from db import db
 
 
-def products(category):
-   
-    if category:
-        filtered_products = Product.query.filter_by(category=category).all()
-        category= category
-        return render_template( 'products.html', **locals() )
-        
-    filtered_products = Product.query.all()
-    print(filtered_products)
-    return render_template( 'products.html', filtered_products=filtered_products )
+def products(argument):
+    filter_by_category = Product.query.filter_by(category=argument).all()
+    if filter_by_category:
+        filtered_products = filter_by_category
+        return render_template( 'products.html', filtered_products=filtered_products)
+    
+    filtered_product = Product.query.filter_by(id=argument).one()
+    return render_template( 'product.html', filtered_product=filtered_product )

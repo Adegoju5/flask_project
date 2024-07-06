bbc0f7f6e4e964ec97105c2c623c6662e9f8ab8f
Adegoju5 <115003200+Adegoju5@users.noreply.github.com>
2024-07-06 16:24:44 +0200

    changes

--- routes/home_route.py
+++ routes/home_route.py
@@ -1,9 +1,18 @@
 from flask import render_template, session
+from models.products import Product
+from db import db
+import random
 
 def home():
     total = session.get('total', {})
     no_of_cartItems = total.get('no_of_items', 0)
-    no_of_cartItems= no_of_cartItems if no_of_cartItems else ''
-    return render_template('home_page.html', no_of_cartItems=no_of_cartItems)
+    no_of_cartItems = no_of_cartItems if no_of_cartItems else ''
+    products = Product.query.all()
+    if len(products) >= 6:
+        random_products = random.sample(products, 6)
+    else:
+        random_products = products 
+
+    return render_template('home_page.html', products=random_products, no_of_cartItems=no_of_cartItems)
 
 
